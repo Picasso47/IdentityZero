629925365f4b1c7388d39ae477da2e14dd30b475
Picasso47 <107099874+Picasso47@users.noreply.github.com>
2025-01-19 15:30:51 +0500

    Create meowhehe.asm

---
+++ Jan2025/AssemblyCodes/Winexec/meowhehe.asm
@@ -0,0 +1,30 @@
+section .data
+my_str db "calc.exe",0
+
+section .bss
+
+section .text
+    global _start
+
+_start:
+    xor rbx,rbx
+    mov bl,60h
+    push QWORD gs:[rbx]     ;pointer to PEB
+    pop rbx
+    mov rbx,[rbx+0x18]      ;PEB+0x18->LDR
+    mov rbx,[rbx+0x20]      ;LDR+0x20h->inmemordermodulelist struct(flink) -> ptr to my.exe
+    push QWORD [rbx]        ;flink of my.exe (ntdll)
+    pop rbx                 ;inmemordermodulelist of 1st LDR_DATA_TABLE_ENTRY->ntdll.dll
+    push QWORD [rbx]        ;flink of ntdll.dll (kernel32)
+    pop rbx                 ;inmemordermodulelist of 1st LDR_DATA_TABLE_ENTRY->kernel32.dll
+    mov rbx,[rbx+0x20]      ;base address of kernel32.dll
+    xor rdx,rdx
+    mov edx,0x70B           ;bx=WinexecAddress-Kernel32BaseAddress -> 70B00
+    shl edx,8               ;70B --> 70B00
+    add rbx,rdx             ;Winexec
+    sub rsp,16
+    xor rcx,rcx
+    mov rcx,my_str          ;LPCSTR (rcx->64bit)
+    xor rdx,rdx             
+    inc rdx
+    call rbx
